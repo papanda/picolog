c12fb1ececc667ad58c6b6090f626c5d4d4492ae
papanda <ichitanister@gmail.com>
2012-04-07 13:10:57 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picolog::Application.config.secret_token = 'eb534691a78dcee23a2bd4159d9dc8dd5a31fd418b4b498f52e0eb8ceed2ed7c37f99328a0daac9a5cda7e7c50b0c79cf1dfda4acf3e37f316f5857abf749546'
