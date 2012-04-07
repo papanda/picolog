c91d95c5f14f82a5068f5ab6a36ec5ec2e849bb5
papanda <ichitanister@gmail.com>
2012-04-07 14:36:33 +0900

    アプリケーションレイアウトがダメ

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
 		auth = request.env["omniauth.auth"]
 		user = User.where(:provider => auth["provider"], :uid => auth["uid"]).first || User.create_with_omniauth(auth)
 		sesion[:user_id] = user.id
-		redirest_to :root, :notice => "login"
+		redirect_to :root, :notice => "login"
 	end
 	def destroy
 		session[:user_id] = nil
-		redirest_to :root, :notice => "logout"
+		redirect_to :root, :notice => "logout"
 	end
 end
