714bd7aa0c695dc3964c6f859e3ac7a878be70a7
papanda <ichitanister@gmail.com>
2012-04-07 14:41:08 +0900

    type

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 	def callback
 		auth = request.env["omniauth.auth"]
 		user = User.where(:provider => auth["provider"], :uid => auth["uid"]).first || User.create_with_omniauth(auth)
-		sesion[:user_id] = user.id
+		session[:user_id] = user.id
 		redirect_to :root, :notice => "login"
 	end
 	def destroy
